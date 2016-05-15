519562c925c544e876bd1713a0844dce880c2322
Josep Ballester <josepbg.git@gmail.com>
2016-05-15 19:55:56 +0200

    Add e3

---
+++ e3/e3.py
@@ -0,0 +1,114 @@
+from ply import lex, yacc
+
+reserved = {
+    'code': 'CODE',
+    'tapes': 'TAPES',
+    'write': 'WRITE',
+    'move': 'MOVE',
+    'state': 'STATE'
+}
+
+# Tokens
+tokens = [
+    'HYPHENS',
+    'DOTS',
+    'COLON',
+    'ID',
+    'STRING',
+    'NUMBER',
+    'INDENT',
+    'DEDENT'
+] + list(reserved.values())
+
+t_HYPHENS   = r'---'
+t_DOTS      = r'\.\.\.'
+t_COLON     = r':'
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+
+def t_STRING(t):
+    r"'.*?'"
+    t.value = t.value.strip("'")
+    return t
+
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+indent_stack = [0]
+
+def t_newline(t):
+    r'\n[ ]*'
+    global indent_stack
+    t.lexer.lineno += 1
+    indent = len(t.value) - 1
+    prev_indents = len(indent_stack)
+
+    if indent_stack[prev_indents - 1] < indent:
+
+        indent_stack.append(indent)
+        t.type = 'INDENT'
+        t.value = indent
+        return t
+
+    elif indent_stack[prev_indents - 1] > indent:
+
+        if indent_stack[prev_indents - 2] >= indent:
+            if indent_stack[prev_indents - 2] > indent:
+                t.lexer.lexpos = t.lexpos
+
+            indent_stack.pop()
+            t.type = 'DEDENT'
+            t.value = indent_stack[prev_indents - 2]
+            return t
+        else:
+            print('Indentation error')
+
+
+t_ignore = ' '
+
+def t_error(t):
+    print('Illegal character "{}"'.format(t.value[0]))
+    t.lexer.skip(1)
+
+
+# Build the lexer
+lexer = lex.lex()
+
+input_file = open('testInput.txt', 'r')
+lexer.input(input_file.read())
+input_file.close()
+
+for tok in lexer:
+    print(tok)
+
+
+def p_main_program(p):
+    'main_program : HYPHENS code_section tapes_section DOTS'
+    print(p)
+
+
+def p_code_section(p):
+    'code_section : CODE COLON INDENT DEDENT'
+
+
+def p_tapes_section(p):
+    'tapes_section : TAPES COLON INDENT DEDENT'
+
+
+def p_error(t):
+    print('Syntax error at "{}"'.format(t.value))
+
+
+parser = yacc.yacc()
+input_file = open('testInput.txt', 'r')
+parser.parse(input_file.read())
+input_file.close()
